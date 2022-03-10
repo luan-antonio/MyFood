2638e4b3c8b6b22816833a07e9de689bffbcad19
luan-antonio <46446054+luan-antonio@users.noreply.github.com>
2022-03-09 22:35:43 -0300

    feat(header): add header component

---
+++ src/components/Layout/Header/index.js
@@ -0,0 +1,12 @@
+import * as S from "./styles";
+
+const Header = (props) => {
+  return (
+    <S.Header>
+        <h1>MyFood</h1>
+        <button>Cart</button>
+    </S.Header>
+  );
+};
+
+export default Header;
