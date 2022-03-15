0c025b58f96041d1bbf155624db5b0811e0b7108
luan-antonio <46446054+luan-antonio@users.noreply.github.com>
2022-03-15 13:19:41 -0300

    feat(Meals): add Meals component

--- src/App.js
+++ src/App.js
@@ -1,13 +1,17 @@
-import { Fragment } from 'react';
+import { Fragment } from "react";
 
 import Header from "./components/Layout/Header";
 import MainImage from "./components/Layout/MainImage";
+import Meals from "./components/Meals";
 
 function App() {
   return (
     <Fragment>
       <Header />
       <MainImage />
+      <main>
+        <Meals />
+      </main>
     </Fragment>
   );
 }
